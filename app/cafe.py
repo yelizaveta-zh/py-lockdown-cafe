9dfc7b83759121d87fddce891d79293d123cac77
Lisa <zhyganiuk.elizaveta@gmail.com>
2024-12-16 17:45:49 +0200

    solution_version_3.0

--- app/cafe.py
+++ app/cafe.py
@@ -15,19 +15,19 @@ class Cafe:
 
         if "vaccine" not in visitor:
             raise NotVaccinatedError(
-                f"{visitor["name"]} is not vaccinated."
+                "Visitor is not vaccinated."
             )
 
         expiration_date = visitor["vaccine"].get("expiration_date")
 
         if expiration_date < datetime.date.today():
             raise OutdatedVaccineError(
-                f"{visitor["name"]}'s" f"vaccine is outdated."
+                "Visitor vaccine is outdated."
             )
 
         if not visitor.get("wearing_a_mask", False):
             raise NotWearingMaskError(
-                f"{visitor["name"]}" f"is not wearing a mask."
+                "Visitor  is not wearing a mask."
             )
 
         return f"Welcome to {self.name}"
