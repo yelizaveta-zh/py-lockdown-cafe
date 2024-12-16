baebdd17fcc28ab0aa46829f4f56cad54ee8fda9
Lisa <zhyganiuk.elizaveta@gmail.com>
2024-12-16 16:49:56 +0200

    solution_version_1.0

--- app/main.py
+++ app/main.py
@@ -1,1 +1,58 @@
-# write your code here
+import datetime
+
+from app.cafe import Cafe
+
+from app.main import go_to_cafe
+
+friends1 = [
+    {
+        "name": "Alisa",
+        "vaccine": {
+            "expiration_date": datetime.date.today()
+        },
+        "wearing_a_mask": True
+    },
+    {
+        "name": "Bob",
+        "vaccine": {
+            "expiration_date": datetime.date.today()
+        },
+        "wearing_a_mask": True
+    },
+]
+print(go_to_cafe(friends1, Cafe("KFC")))
+
+
+friends2 = [
+    {
+        "name": "Alisa",
+        "vaccine": {
+            "expiration_date": datetime.date.today()
+        },
+        "wearing_a_mask": False
+    },
+    {
+        "name": "Bob",
+        "vaccine": {
+            "expiration_date": datetime.date.today()
+        },
+        "wearing_a_mask": False
+    },
+]
+print(go_to_cafe(friends2, Cafe("KFC")))
+
+
+friends3 = [
+    {
+        "name": "Alisa",
+        "wearing_a_mask": True
+    },
+    {
+        "name": "Bob",
+        "vaccine": {
+            "expiration_date": datetime.date.today()
+        },
+        "wearing_a_mask": True
+    },
+]
+print(go_to_cafe(friends3, Cafe("KFC")))
